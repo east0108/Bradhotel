452d0416592432cf7d4381a9fd0b925fe7b80684
east0108 <abc1236107@gmail.com>
2023-08-31 17:09:23 +0800

    初始化 Spring Boot project

---
+++ src/test/java/com/hotel/bradhotel/BradhotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.bradhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BradhotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
